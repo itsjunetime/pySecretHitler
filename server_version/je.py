70041049d9b9aa591b78beef7e6d19f78f16a73a
Ian Welker <iandwelker@gmail.com>
2020-05-18 20:29:24 -0600

    Basic JSON & edit locking

---
+++ server_version/je.py
@@ -0,0 +1,68 @@
+import json
+import os
+import time
+import random
+
+def lock():
+    try: 
+        open('.ed.lock', 'x')
+        return True
+    except:
+        t=0
+        while os.path.exists('.ed.lock'):
+            time.sleep(0.1)
+            t+=1
+            if t == 20:
+                return False
+        else:
+            open('.ed.lock', 'x')
+            return True
+
+def unlock():
+    os.remove('.ed.lock')
+
+def insert(key, val):
+    lock()
+    with open('stats.json', 'r+') as f:
+        json_data = json.load(f)
+        json_data[key] = val
+        f.seek(0)
+        f.write(json.dumps(json_data, indent=4, sort_keys=True))
+        f.truncate()
+    time.sleep(1)
+    unlock()
+
+def getDeck():
+    with open('stats.json', 'r') as f:
+        json_data = json.load(f)
+        return json_data['deck']
+
+def getDiscard():
+    with open('stats.json', 'r') as f:
+        json_data = json.load(f)
+        return json_data['discard']
+
+def threePolicies():
+    lock()
+    with open('stats.json', 'r+') as f:
+        json_data = json.load(f)
+        nums = []
+        for i in range(3):
+            numtoadd = random.randint(0, len(deck) - 1)
+            while numtoadd in nums:
+                numtoadd = random.randint(0, len(deck) - 1)
+            else:
+                nums+=numtoadd
+
+        nums.sort(reverse-True)
+        to_return = []
+        for i in nums:
+            to_return+=json_data['deck'].pop(i)
+
+        f.seek(0)
+        f.write(json.dumps(json_data))
+        f.truncate()
+    unlock()
+
+insert("last_action", "")
+print(getDeck())
